88b791bcb49a0da42cb9292853ef29937c232b2a
ErickICL <64990111+ErickICL@users.noreply.github.com>
2020-11-05 19:12:54 -0700

    Create salvador.blade.php

---
+++ salvador.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.master')
+@section('title', 'Salvador Dalí')
+@section('header')
+@stop
+@section('navbar')
+@show
+@section('contenido')
+<h1>Salvador Dalí</h1>
+    <div class = "contenedor">
+    <p>Salvador Dalí es conocido por sus impactantes y oníricas imágenes surrealistas. Sus habilidades pictóricas se suelen atribuir a la influencia y admiración por el arte renacentista. También fue un experto dibujante. Los recursos plásticos dalinianos también abordaron el cine, la escultura y la fotografía, lo cual le condujo a numerosas colaboraciones con otros artistas audiovisuales. Tuvo la capacidad de acrisolar un estilo genuinamente personal y palpable al primer contacto, que en realidad era muy ecléctico y que «succionó» de innovaciones ajenas. Una de sus pinturas más célebres es La persistencia de la memoria (también conocida como Los relojes blandos), realizada en 1931.
+
+Como artista extremadamente imaginativo, manifestó una notable tendencia al narcisismo y la megalomanía, cuyo objeto era atraer la atención pública. Esta conducta irritaba a quienes apreciaban su arte y justificaba a sus críticos, que rechazaban sus conductas excéntricas como un reclamo publicitario ocasionalmente más llamativo que su producción artística.​ Dalí atribuía su «amor por todo lo que es dorado y resulta excesivo, su pasión por el lujo y su amor por la moda oriental a un autoproclamado «linaje arábigo, que remontaba sus raíces a los tiempos de la dominación árabe de la península ibérica.</p>
+    </div>
+    <table width="100%" border="0" cellpadding="1">
+    <table>
+        <br><hr>
+        @stop
+@section('footer')
+@parent
+@stop
