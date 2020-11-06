a4c60ffac13c67cfcf6058e34adbe3c0c51b91fb
ErickICL <64990111+ErickICL@users.noreply.github.com>
2020-11-05 19:05:56 -0700

    Create footer.blade.php

---
+++ includes/footer.blade.php
@@ -0,0 +1,8 @@
+<br>
+<footer style="background-color: black">
+@section('footer')
+<center style="color: white"> Ceniceros Luna Erick Iván {{date('Y')}}</center>
+@show
+    </footer>
+</body>
+</html> 
