afc7e326694003bb96dd7675ab1de010536d474b
ErickICL <64990111+ErickICL@users.noreply.github.com>
2020-11-05 19:11:05 -0700

    Create lenardo.blade.php

---
+++ lenardo.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.master')
+@section('title', 'Leonardo Da Vinci')
+@section('header')
+@stop
+@section('navbar')
+@show
+@section('contenido')
+<h1>Leonardo Da Vinci</h1>
+    <div class="contenedor">
+        <p>Pintor, inventor, escultor, arquitecto, ingeniero, astrónomo... Clasificar al italiano Leonardo da Vinci es una sola disciplina es una tarea imposible. Su figura encarna perfectamente el ideal del hombre del Renacimiento, y su expresión derivada como sinónimo de polímatía, o sabiduría aplicada a diversos campos del arte y de la ciencia. Como inventor dejó trazadas las ideas de lo que luego serían dispositivos y aparatos tan dispares como el paracaídas, el helicóptero y el tanque, aunque el nivel tecnológico de su época hizo imposible que los viera convertidos en realidad. Sin duda, su legado más universal está en el campo de la pintura. Obras magnas como La última cena o La dama del armiño han quedado relativamente eclipsadas por la dimensión de su cuadro más famoso: la Mona Lisa, o Gioconda, expuesta en el museo del Louvre de París y convertida en un auténtico icono popular. </p>
+    </div>
+    <table width="100%" border="0" cellpadding="1">
+    <table>
+        <br><hr>
+        @stop
+@section('footer')
+@parent
+@stop
