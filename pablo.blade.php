bde35964eb8230807902bc3c39c9a89790c98adb
ErickICL <64990111+ErickICL@users.noreply.github.com>
2020-11-05 19:11:29 -0700

    Create pablo.blade.php

---
+++ pablo.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.master')
+@section('title', 'Pablo Picasso')
+@section('header')
+@stop
+@section('navbar')
+@show
+@section('contenido')
+<h1>Pablo Picasso</h1>
+    <div class="contenedor">
+        <p>Pablo Picasso es, sin lugar a dudas, uno de los artistas plásticos más famosos del mundo. Su obra trasciende todo tipo de fronteras y de clasificaciones; tan vasta y diversificada como única, supuso un antes y un después en la producción artística a nivel mundial. Polémico, controvertido, con un talento sin límites y una insaciable necesidad de crear, su frase más conocida refleja fielmente su personalidad: <u>“cuando llegue la inspiración, que me encuentre trabajando”.</u> El arte de Picasso es multiforme y cambiante; el artista se nutrió del pasado y del presente para crear sus obras, anticipando lo que vendría años e incluso décadas después. Porque el arte contemporáneo jamás sería lo que hoy es sin la influencia de Pablo Picasso: un artista que pasó por su época como un tren sin frenos, arrollando las fronteras del arte del siglo XX y abriendo nuevos e insospechados caminos creativos.</p>
+    </div>
+    <table width="100%" border="0" cellpadding="1">
+    <table>
+        <br><hr>
+        @stop
+@section('footer')
+@parent
+@stop
