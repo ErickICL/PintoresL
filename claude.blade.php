4c58f3227ba93092a604af39d0dc1fc73c7160a9
ErickICL <64990111+ErickICL@users.noreply.github.com>
2020-11-05 19:09:51 -0700

    Create claude.blade.php

---
+++ claude.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.master')
+@section('title', 'Claude Monet')
+@section('header')
+@stop
+@section('navbar')
+@show
+@section('contenido')
+<h1>Claude Monet</h1>
+    <div class = "contenedor">
+    <p>Claude Monet fue un pintor francés, uno de los creadores del impresionismo. El término impresionismo deriva del título de su obra Impresión, sol naciente (1872).
+
+Sus primeras obras, hasta la mitad de la década de 1860, son de estilo realista. Monet logró exponer algunas en el Salón de París. A partir del final de la década de 1860 comenzó a pintar obras impresionistas. Esta desviación del gusto de la época, que era marcado por las academias de arte, empeoró su situación económica a la vez que afianzó su decisión de continuar en ese azaroso camino.
+
+En la década de 1870 formó parte de las exposiciones impresionistas en las cuales también participaron Pierre-Auguste Renoir y Edgar Degas. Su obra Impresión, sol naciente formó parte del Salon des Refusés de 1874. Su carrera fue impulsada por el marchante Paul Durand-Ruel, pero a pesar de esto su situación financiera permaneció siendo difícil hasta mediados de la década de 1890. En esta época, Monet desarrolló el concepto de las «series», en las que un motivo es pintado repetidas veces con distinta iluminación. Al mismo tiempo comenzó a trabajar en el famoso jardín de su casa en Giverny con estanques de nenúfares que luego utilizó como motivo para sus pinturas.</p>
+    </div>
+    <table width="100%" border="0" cellpadding="1">
+    <table>
+        <br><hr>
+        @stop
+@section('footer')
+@parent
+@stop
